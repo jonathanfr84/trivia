03e008e0abfccb35c5995edb35978e25ea12cfd4
jfernaro <jonathanfr84@gmail.com>
2016-03-20 18:47:07 +0100

    - Se agrega test para validar que cuando se agrega un jugador, se escribe su nombre y número de jugador. - Se agrega un setUp para no crear "game" en cada test

--- java/com/adaptionsoft/games/tests/GameTests.java
+++ java/com/adaptionsoft/games/tests/GameTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.adaptionsoft.games.uglytrivia.Game;
@@ -13,22 +14,25 @@ import com.adaptionsoft.games.trivia.runner.GameRunner;
 
 public class GameTests {
 	
-	@Test
-	public void new_game_is_created_correctly() {
-		Game game = new Game();
-		assertEquals("", "");
+	private Game game;
+	
+	@Before
+	public void setUp() {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(stream);
+		System.setOut(printStream);
+		game = new Game();
+		assertEquals("", stream.toString());
 	}
 	
 	@Test
 	public void one_player_can_be_added() {
-		Game game = new Game();
 		game.add("Jonathan");
 		assertEquals(1, game.howManyPlayers());
 	}
 	
 	@Test
 	public void two_players_can_be_added() {
-		Game game = new Game();
 		game.add("Jonathan");
 		game.add("Paco");
 		assertEquals(2, game.howManyPlayers());
@@ -37,7 +41,6 @@ public class GameTests {
 	//Se trata de una limitación del código. Denería aceptar más de 7
 	@Test(expected=ArrayIndexOutOfBoundsException.class)
 	public void when_seven_players_are_added_get_an_excepction() {
-		Game game = new Game();
 		game.add("Jonathan");
 		game.add("Paco");
 		game.add("Antonio");
@@ -48,9 +51,19 @@ public class GameTests {
 		assertEquals(7, game.howManyPlayers());
 	}
 	
+	@Test
+	public void when_player_is_added_his_name_is_written() {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(stream);
+		System.setOut(printStream);
+		game.add("Jonathan");
+		assertEquals("Jonathan was added\r\n" + 
+				"They are player number 1\r\n", stream.toString());
+		System.setOut(System.out);
+	}
+	
 	@Test(expected=IndexOutOfBoundsException.class)
 	public void when_player_is_removed_get_an_exception() {
-		Game game = new Game();
 		game.add("Jonathan");
 		game.remove("Jonathan");
 		assertEquals(0, game.howManyPlayers());
