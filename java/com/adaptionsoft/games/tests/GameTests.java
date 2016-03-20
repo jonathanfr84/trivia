8d13ad73951cbc9be379db5fdee2a020a9f421b4
jfernaro <jonathanfr84@gmail.com>
2016-03-20 19:21:49 +0100

    - Corrección setUp - Se agrega test de lanzar dados con resultados 1 y 3.

--- java/com/adaptionsoft/games/tests/GameTests.java
+++ java/com/adaptionsoft/games/tests/GameTests.java
@@ -14,14 +14,42 @@ import com.adaptionsoft.games.trivia.runner.GameRunner;
 
 public class GameTests {
 	
+	private static final String ROLL_WITH_THREE = "Chet was added\r\n" + 
+			"They are player number 1\r\n" + 
+			"Pat was added\r\n" + 
+			"They are player number 2\r\n" + 
+			"Sue was added\r\n" + 
+			"They are player number 3\r\n" + 
+			"Chet is the current player\r\n" + 
+			"They have rolled a 3\r\n" + 
+			"Chet's new location is 3\r\n" + 
+			"The category is Rock\r\n" + 
+			"Rock Question 0\r\n";
+
+	private static final String ROLL_WITH_ONE = "Chet was added\r\n" + 
+			"They are player number 1\r\n" + 
+			"Pat was added\r\n" + 
+			"They are player number 2\r\n" + 
+			"Sue was added\r\n" + 
+			"They are player number 3\r\n" + 
+			"Chet is the current player\r\n" + 
+			"They have rolled a 1\r\n" + 
+			"Chet's new location is 1\r\n" + 
+			"The category is Science\r\n" + 
+			"Science Question 0\r\n";
+	
 	private Game game;
 	
 	@Before
 	public void setUp() {
+		game = new Game();
+	}
+	
+	@Test
+	public void game_is_created_correctly() {
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(stream);
 		System.setOut(printStream);
-		game = new Game();
 		assertEquals("", stream.toString());
 	}
 	
@@ -68,4 +96,34 @@ public class GameTests {
 		game.remove("Jonathan");
 		assertEquals(0, game.howManyPlayers());
 	}
+	
+	@Test
+	public void one_roll_with_result_one() {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(stream);
+		System.setOut(printStream);
+		Game game = new Game();
+		game.add("Chet");
+		game.add("Pat");
+		game.add("Sue");
+		game.roll(1);
+		assertEquals(ROLL_WITH_ONE, stream.toString());
+
+	}
+	
+	@Test
+	public void one_roll_with_result_three() {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(stream);
+		System.setOut(printStream);
+		Game game = new Game();
+		game.add("Chet");
+		game.add("Pat");
+		game.add("Sue");
+		game.roll(3);
+		assertEquals(ROLL_WITH_THREE, stream.toString());
+
+	}
+	
+	
 }
