8c178998d5b13cbfdc1ecea3e17f946c6421c756
jonathanfr84 <jonathanfr84@gmail.com>
2016-03-20 19:49:46 +0100

    Se elimina código que no se utiliza en la aplicación: · class SimpleSingleton · main que imprime "Hello world".

--- java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -108,10 +108,7 @@ public class Game {
 		if (currentCategory() == "Rock")
 			System.out.println(rockQuestions.removeFirst());		
 	}
-	
-  public static void main(String[] args) {
-    System.out.println("Hello World!"); // Display the string.
-  }
+
 
 	// randomly return a category
 	private String currentCategory() {
@@ -177,20 +174,6 @@ public class Game {
 		return true;
 	}
 
-	public static class SimpleSingleton {
-    private static SimpleSingleton singleInstance =  new SimpleSingleton();
- 
-    //Marking default constructor private
-    //to avoid direct instantiation.
-    private SimpleSingleton() {
-    }
- 
-    //Get instance for class SimpleSingleton
-    public static SimpleSingleton getInstance() {
- 
-        return singleInstance;
-  }
-}
 	/**
 	 * Tells if the last player won.
 	 */
